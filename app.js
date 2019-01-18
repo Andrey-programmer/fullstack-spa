6c7a9f89c9b3deffae84ab5028c0de92a8801210
Andrey_rogrammer <lak0sta1302@gmail.com>
2019-01-18 12:15:48 +0300

    подключены библиотеки cors и morgan - для визуализации запросов в cmd

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
 const authRoutes = require('./Routes/auth')
 const analyticsRoutes = require('./Routes/analytics')
 const categoryRoutes = require('./Routes/category')
@@ -7,8 +10,10 @@ const orderRoutes = require('./Routes/order')
 const positionRoutes = require('./Routes/position')
 const app = express()
 
+app.use(morgan('dev')) //говорит о том что мы находимся в режиме разработки
 app.use(bodyParser.urlencoded({extended: true}))//Позволяет правильно парсить символы в URL-адресе
 app.use(bodyParser.json()) //перевод json из JS в postman
+app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
