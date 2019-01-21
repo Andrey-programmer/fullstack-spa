f46aeb1d240e3decfe8ccb84d70702a2f8c1b556
Andrey_programmer <Lak0sta1302@gmail.com>
2019-01-21 14:54:33 +0300

    Получаем токен при верной серверной авторизации

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,14 +1,52 @@
-const bycrpt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = function(request, response) {
-    response.status(200).json({
+module.exports.login = async function(request, response) {
+    
+    //ищем пользователя по email в базе данных
+    const candidate = await User.findOne({
+        email: request.body.email
+    })
+
+    if (candidate) {
+        //Проверка пароля пользователя (локальный пароль, глобальный пароль)
+        const passwordResult = bcrypt.compareSync(request.body.password, 
+        candidate.password)
+
+        if (passwordResult) {
+            //Генерируем токен, пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60*60}) //60*60 - токен будет действовать час
+
+            response.status(200).json({
+                token: `Bearer ${token}` //Bearer - необходимо для правильно синтаксиса
+            })
+
+        } else {
+            // Пароли несовпали
+            response.status(401).json({
+                message: 'Пароли несовпадают. Попробуйте снова.'
+            })
+        }
+
+    } else {
+        //Пользователя нет. ошибка
+        response.status(404).json({
+            message: 'Пользователь с таким email не найден!'
+        })
+    } 
+
+    /* response.status(200).json({
         login: {
             email: request.body.email ,
             password: request.body.password
         }
-    })
+    }) */
 }
 
 module.exports.register =  async function(request, response) {
@@ -27,13 +65,13 @@ module.exports.register =  async function(request, response) {
        //Нужно создать пользователя
 
         //генерируем хэш на 10 символов
-       const salt =  bycrpt.genSaltSync(10)
+       const salt =  bcrypt.genSaltSync(10)
       
        const password = request.body.password
 
        const user = new User({
            email: request.body.email,
-           password: bycrpt.hashSync(password, salt)
+           password: bcrypt.hashSync(password, salt)
        })
 
        try {
