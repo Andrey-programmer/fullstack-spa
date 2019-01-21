68a490d318500d70276ed809d3e7b73a2c091f83
Andrey_programmer <Lak0sta1302@gmail.com>
2019-01-21 14:03:56 +0300

    Осуществлена регистрация с проверкой пользователя на уже зарегистрированного

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,3 +1,5 @@
+const bycrpt = require('bcryptjs')
+
 const User = require('../models/User')
 
 module.exports.login = function(request, response) {
@@ -9,13 +11,47 @@ module.exports.login = function(request, response) {
     })
 }
 
-module.exports.register = function(request, response) {
-    const user = new User({
+module.exports.register =  async function(request, response) {
+   
+    //Проверяем существует ли пользователь
+   const candidate = await User.findOne({
+       email: request.body.email
+   })
+
+   if (candidate) {
+       //Пользователь существует - выдаем ошибку
+       response.status(409).json({
+           message: 'Такой email уже занят!'
+       })
+   } else {
+       //Нужно создать пользователя
+
+        //генерируем хэш на 10 символов
+       const salt =  bycrpt.genSaltSync(10)
+      
+       const password = request.body.password
+
+       const user = new User({
+           email: request.body.email,
+           password: bycrpt.hashSync(password, salt)
+       })
+
+       try {
+            await user.save()
+            response.status(201).json(user) 
+       } catch(error) {
+           //Обрабатываем ошибку
+           
+       }
+       
+   }
+
+    /*  const user = new User({
         email: request.body.email,
         password: request.body.password
     })
  
     user.save().then(() => {
         console.log('User created')
-    }) 
+    })  */
 }
