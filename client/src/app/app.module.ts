e1f8c0ba691300f79ff8872d8e69c2fb45d7240f
Andrey-programmer <Lak0sta1302@gmail.com>
2019-02-01 17:52:31 +0300

    Реализована валидация формы на странице авторизации

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { LoginPageComponent } from './login-page/login-page.component';
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SystemLayoutComponent } from './shared/layouts/system-layout/system-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component'
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component'
+import { SystemLayoutComponent } from './shared/layouts/system-layout/system-layout.component'
 import { RegisterPageComponent } from './register-page/register-page.component'
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { RegisterPageComponent } from './register-page/register-page.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
