b2ce658033a29fb8b4a096c047b6da04fde13506
Andrey-programmer <Lak0sta1302@gmail.com>
2019-02-01 15:47:26 +0300

    Оформлен слой авторизации, простые  ссылки замененены на SPA-ссылки

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { LoginPageComponent } from './login-page/login-page.component'
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SystemLayoutComponent } from './shared/layouts/system-layout/system-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SystemLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
