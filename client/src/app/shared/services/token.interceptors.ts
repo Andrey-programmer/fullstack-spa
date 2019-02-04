a9557b3a04cea95716e8f3d7bd0201772892c22f
Andrey-programmer <Lak0sta1302@gmail.com>
2019-02-04 17:01:04 +0300

    Добавлены интерцептеры и сохранение токена в localstorage

---
+++ client/src/app/shared/services/token.interceptors.ts
@@ -0,0 +1,24 @@
+import { AuthService } from './auth.service'
+import { Injectable } from '@angular/core'
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+    constructor(private auth: AuthService) {
+
+    }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if(this.auth.isAuthenticated()) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: this.auth.getToken()
+                }
+            })
+        }
+        
+        return next.handle(request)
+    }
+}
