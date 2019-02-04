72eb14218cd77e5c2b9f79842962f3f88016eade
Andrey-programmer <Lak0sta1302@gmail.com>
2019-02-04 12:48:18 +0300

    Добавлена авторизация пользователя в mlab через страницу регистрации

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { AuthService } from '../shared/services/auth.service'
+import { User } from '../shared/interfaces/interfaces'
+import { Router } from '@angular/router'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup
+  aSub: Subscription
+  constructor(private auth: AuthService, private router: Router) { }
+
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]
+      )
+    })
   }
 
+  onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.register(this.form.value).subscribe((user: User) => {
+      this.router.navigate(['/login'], {queryParams: {
+        registered: true
+      }})
+    },
+    error => {
+      console.log(JSON.stringify(error.error.message))
+        //Включаем форму если ошибка авторизации
+        this.form.enable()
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.aSub) {
+      this.aSub.unsubscribe()
+    }
+  }
 }
